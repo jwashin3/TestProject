96a8312dc3262082a9e03efa897f5204f116b17a
j <j@DESKTOP-090UK8B>
2021-04-10 14:57:58 -0500

    number input

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Here is some output for the input");
+		int a = scan.nextInt();
+		System.out.println(a);
 
 	}
 
