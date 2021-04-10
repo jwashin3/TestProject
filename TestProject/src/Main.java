a534804e6566daee01d71c2193dbd02675b2c4b1
j <j@DESKTOP-090UK8B>
2021-04-10 16:04:26 -0500

    if statement

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -5,10 +5,19 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner (System.in);
-		System.out.println("Here is some output for the input");
-		int a = scan.nextInt();
-		System.out.println(a);
-
-	}
+		
+		System.out.println("Enter two strings, I'll tel you if they're equal");
+		
+		String string1 = scan.nextLine();
+		String string2 = scan.nextLine();
+		
+		if (string1.equals(string2)) {
+			System.out.println("They are equal");
+		}
+		else {
+			System.out.println("They are not equal");
+		}
+		
+}
 
 }
